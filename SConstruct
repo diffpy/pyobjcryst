d2bff51a55a99c7dcf5d4cb26c9d296bea4d7010
Christopher Farrow <farrowch@gmail.com>
2009-06-12 18:47:04 +0000

    Adding partial ObjCryst build setup. Refactoring will follow.

--- SConstruct
+++ SConstruct
@@ -1,15 +1,5 @@
 import os.path
 
-# Declare location of objcryst files
-AddOption('--objcrystheaders',
-          dest='objcrystheaders',
-          type='string',
-          nargs=1,
-          action='store',
-          metavar='DIR',
-          default='',
-          help='Location of Objcryst header files')
-
 # Auxillary library location
 AddOption('--auxlib',
           dest='auxlib',
@@ -23,12 +13,10 @@ AddOption('--auxlib',
 
 env = DefaultEnvironment()
 
-env.Append(CPPPATH = GetOption("objcrystheaders"))
 env.Append(LIBPATH = GetOption("auxlib"))
-
-env.ParseConfig("python-config --includes")
-env.ParseConfig("python-config --ldflags")
 
 Export('env')
 
-SConscript("boost/SConscript")
+SConscript(["src/SConscript", "boost/SConscript"])
+
+# We should have
--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@
 from setuptools import setup, find_packages
 import fix_setuptools_chmod
 
-#So we can build scons stuff
-import sys
-setup_requires = ['openalea.deploy']
-# web sites where to find eggs
-dependency_links = ['http://openalea.gforge.inria.fr/pi']
+# OpenAlea.deploy extends setuptools in order to support scons setups, and
+# various other features.
+openalea_requires = ['openalea.deploy']
+openalea_links = ['http://openalea.gforge.inria.fr/pi']
 
 # define distribution
 dist = setup(
         name = "pyobjcryst",
         version = "0.1a1",
         author = "Christopher L. Farrow",
-        packages = find_packages(),
+        packages = ['pyobjcryst'],
         author_email = "clf2121@columbia.edu",
         description = "Bindings of ObjCryst++ into python",
         license = "BSD",
         url = "http://www.diffpy.org/",
 
-        # openalea
-        setup_requires = setup_requires,
-        dependency_links = dependency_links,
+        # These will allow us to use the OpenAlea build system without any
+        # effort.
+        setup_requires = openalea_requires,
+        dependency_links = openalea_requires,
 
-        # scons
+        # Now we can tell distutils what to install
         scons_scripts=['SConstruct']
 )
 
