5621370483c4dd49d3fdfd2a32308fcbe0dc5538
Pavol Juhas <pavol.juhas@gmail.com>
2015-05-07 15:54:05 -0400

    Format post-release versions as per PEP 440.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def create_extensions():
 
 # Use this version when git data are not available, like in git zip archive.
 # Update when tagging a new release.
-FALLBACK_VERSION = '1.0-x'
+FALLBACK_VERSION = '1.0.post0'
 
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
@@ -78,7 +78,7 @@ def gitinfo():
     proc = Popen(['git', 'log', '-1', '--format=%H %at %ai'], **kw)
     glog = proc.stdout.read()
     rv = {}
-    rv['version'] = '-'.join(desc.strip().split('-')[:2]).lstrip('v')
+    rv['version'] = '.post'.join(desc.strip().split('-')[:2]).lstrip('v')
     rv['commit'], rv['timestamp'], rv['date'] = glog.strip().split(None, 2)
     return rv
 
