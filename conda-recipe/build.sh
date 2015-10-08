2db30560e38ab038592dd160710ed0c0b4311e12
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-08 14:21:14 -0400

    Add conda build recipe.
    In sync with pavoljuhas/conda-recipes@70b6bd457f7a611c98cfd61fc0e400722fc67ec0

---
+++ conda-recipe/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export CPATH="$PREFIX/include:$CPATH"
+export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
+
+if [ `uname` == Darwin ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+fi
+
+scons -j $MYNCPU install prefix=$PREFIX
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
