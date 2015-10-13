68278f66e6656e66ccf33db310612cb8c3153829
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-13 18:38:54 -0400

    Use git tag based versioning in conda package.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -12,6 +12,8 @@ scons -j $MYNCPU install prefix=$PREFIX
 
 # Add more build steps here, if they are necessary.
 
+$PYTHON setup.py --version > __conda_version__.txt
+
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
