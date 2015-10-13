55ec455dd2118b79176b4c212d1dec558d706d26
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-13 17:26:45 -0400

    Issue a warning when boost_python is not detected.
    Avoid hard crash and inability to run setup.py.

--- setup.py
+++ setup.py
@@ -34,16 +34,23 @@ def get_boost_libraries():
     thrown.
     """
     baselib = "boost_python"
-    boostlibtags = ['', '-mt']
+    boostlibtags = ['', '-mt'] + ['']
     from ctypes.util import find_library
     for tag in boostlibtags:
         lib = baselib + tag
         found = find_library(lib)
         if found: break
 
-    # Raise Exception if we don't find anything
+    # Show warning when library was not detected.
     if not found:
-        raise Exception("Cannot find shared boost_library library")
+        import platform
+        import warnings
+        ldevname = 'LD_LIBRARY_PATH'
+        if platform.system() == 'Darwin':
+            ldevname = 'DYLD_FALLBACK_LIBRARY_PATH'
+        wmsg = ("Cannot detect name suffix for the %r library.  "
+            "Consider setting %s.") % (baselib, ldevname)
+        warnings.warn(wmsg)
 
     libs = [lib]
     return libs
