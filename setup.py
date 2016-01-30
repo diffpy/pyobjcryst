96c98d229abc65326b89a36468c1297e80dec39b
Pavol Juhas <pavol.juhas@gmail.com>
2016-01-30 14:10:47 -0500

    Version 2.0.1

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ from setuptools import setup
 from setuptools import Extension
 from numpy.distutils.misc_util import get_numpy_include_dirs
 
+# Use this version when git data are not available as in a git zip archive.
+# Update when tagging a new release.
+FALLBACK_VERSION = '2.0.1.post0'
 
 # define extension arguments here
 ext_kws = {
@@ -66,10 +69,6 @@ def create_extensions():
             **ext_kws)
     return [ext]
 
-
-# Use this version when git data are not available, like in git zip archive.
-# Update when tagging a new release.
-FALLBACK_VERSION = '2.0.post0'
 
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
