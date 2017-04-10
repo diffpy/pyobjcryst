6e673c79f03f0e7e75603a68e1df1219949cc8a8
Pavol Juhas <pavol.juhas@gmail.com>
2017-04-10 14:38:52 -0400

    TST: switch to automated TestCase discovery.
    Avoid need to maintain a list of test modules.
    Also add `pattern` argument to testsuite() for filtering test cases.

--- pyobjcryst/tests/__init__.py
+++ pyobjcryst/tests/__init__.py
@@ -16,36 +16,51 @@
 """Unit tests for pyobjcryst.
 """
 
+import unittest
 
-def testsuite():
-    '''Build a unit tests suite for the pyobjcryst package.
 
-    Return a unittest.TestSuite object.
+def testsuite(pattern=''):
+    '''Create a unit tests suite for the pyobjcryst package.
+
+    Parameters
+    ----------
+    pattern : str, optional
+        Regular expression pattern for selecting test cases.
+        Select all tests when empty.
+
+    Returns
+    -------
+    suite : `unittest.TestSuite`
+        The TestSuite object containing the matching tests.
     '''
-    import unittest
-    modulenames = '''
-        pyobjcryst.tests.testcif
-        pyobjcryst.tests.testclocks
-        pyobjcryst.tests.testconverters
-        pyobjcryst.tests.testcrystal
-        pyobjcryst.tests.testmolecule
-        pyobjcryst.tests.testrefinableobj
-        pyobjcryst.tests.testutils
-    '''.split()
-    suite = unittest.TestSuite()
+    import re
+    from itertools import chain
+    from pkg_resources import resource_filename
     loader = unittest.defaultTestLoader
-    mobj = None
-    for mname in modulenames:
-        exec ('import %s as mobj' % mname)
-        suite.addTests(loader.loadTestsFromModule(mobj))
+    thisdir = resource_filename(__name__, '')
+    suite_all = loader.discover(thisdir)
+    # shortcut when pattern is not specified
+    if not pattern:
+        return suite_all
+    # here we need to filter the suite by pattern
+    suite = unittest.TestSuite()
+    rx = re.compile(pattern)
+    tcases = chain.from_iterable(chain.from_iterable(suite_all))
+    for tc in tcases:
+        tcwords = tc.id().rsplit('.', 2)
+        shortname = '.'.join(tcwords[-2:])
+        if rx.search(shortname):
+            suite.addTest(tc)
     return suite
 
 
 def test():
     '''Execute all unit tests for the pyobjcryst package.
-    Return a unittest TestResult object.
+
+    Returns
+    -------
+    result : `unittest.TestResult`
     '''
-    import unittest
     suite = testsuite()
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
