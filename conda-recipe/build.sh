816df44bfb819771a1e105993b16a316528e0ca9
Pavol Juhas <pavol.juhas@gmail.com>
2017-05-02 17:09:18 -0400

    DEV: silence copious boost-warnings in conda build.
    These are the C++11 related warnings originating from boost headers.
    Prevent travis error due to excessive log size.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,12 +2,17 @@
 
 export CPATH="${PREFIX}/include:$CPATH"
 export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
+
 MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 if [ `uname` == Darwin ]; then
     export DYLD_FALLBACK_LIBRARY_PATH="$PREFIX/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 fi
 
+# Apply sconscript.local customizations.
+cp ${RECIPE_DIR}/sconscript.local ./
+
+# Install package with scons to utilize multiple CPUs.
 scons -j $MYNCPU install prefix=$PREFIX
 
 # Add more build steps here, if they are necessary.
