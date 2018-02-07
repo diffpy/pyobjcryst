f71feb0bab379a405a38c973e12ffa3eccf921ad
Pavol Juhas <pavol.juhas@gmail.com>
2018-02-07 12:25:02 -0500

    TST: add blank tests for the powderpattern module

---
+++ src/pyobjcryst/tests/testpowderpattern.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+"""Unit tests for pyobjcryst.powderpattern
+"""
+
+
+import unittest
+
+from pyobjcryst.powderpattern import PowderPattern
+
+# ----------------------------------------------------------------------------
+
+class TestRoutines(unittest.TestCase):
+
+    pass
+    #def test_CreatePowderPatternFromCIF(self):  assert False
+
+# End of class TestRoutines
+
+# ----------------------------------------------------------------------------
+
+class TestPowderPattern(unittest.TestCase):
+
+    pass
+    #def test___init__(self):  assert False
+    #def test_AddPowderPatternBackground(self):  assert False
+    #def test_AddPowderPatternDiffraction(self):  assert False
+    #def test_FitScaleFactorForIntegratedR(self):  assert False
+    #def test_FitScaleFactorForIntegratedRw(self):  assert False
+    #def test_FitScaleFactorForR(self):  assert False
+    #def test_FitScaleFactorForRw(self):  assert False
+    #def test_GetMaxSinThetaOvLambda(self):  assert False
+    #def test_GetNbPowderPatternComponent(self):  assert False
+    #def test_GetPowderPatternCalc(self):  assert False
+    #def test_GetPowderPatternComponent(self):  assert False
+    #def test_GetPowderPatternObs(self):  assert False
+    #def test_GetPowderPatternX(self):  assert False
+    #def test_GetScaleFactor(self):  assert False
+    #def test_ImportPowderPattern2ThetaObs(self):  assert False
+    #def test_ImportPowderPattern2ThetaObsSigma(self):  assert False
+    #def test_ImportPowderPatternFullprof(self):  assert False
+    #def test_ImportPowderPatternFullprof4(self):  assert False
+    #def test_ImportPowderPatternGSAS(self):  assert False
+    #def test_ImportPowderPatternILL_D1A5(self):  assert False
+    #def test_ImportPowderPatternMultiDetectorLLBG42(self):  assert False
+    #def test_ImportPowderPatternPSI_DMC(self):  assert False
+    #def test_ImportPowderPatternSietronicsCPI(self):  assert False
+    #def test_ImportPowderPatternTOF_ISIS_XYSigma(self):  assert False
+    #def test_ImportPowderPatternXdd(self):  assert False
+    #def test_Prepare(self):  assert False
+    #def test_SetEnergy(self):  assert False
+    #def test_SetMaxSinThetaOvLambda(self):  assert False
+    #def test_SetPowderPatternObs(self):  assert False
+    #def test_SetPowderPatternPar(self):  assert False
+    #def test_SetPowderPatternX(self):  assert False
+    #def test_SetScaleFactor(self):  assert False
+    #def test_SetWavelength(self):  assert False
+
+# End of class TestPowderPattern
+
+# ----------------------------------------------------------------------------
+
+class TestPowderPatternComponent(unittest.TestCase):
+
+    pass
+    #def test___init__(self):  assert False
+    #def test_GetParentPowderPattern(self):  assert False
+
+# End of class TestPowderPatternComponent
+
+# ----------------------------------------------------------------------------
+
+class TestPowderPatternBackground(unittest.TestCase):
+
+    pass
+    #def test___init__(self):  assert False
+    #def test_FixParametersBeyondMaxresolution(self):  assert False
+    #def test_GetPowderPatternCalc(self):  assert False
+    #def test_ImportUserBackground(self):  assert False
+    #def test_OptimizeBayesianBackground(self):  assert False
+    #def test_SetInterpPoints(self):  assert False
+
+# End of class TestPowderPatternBackground
+
+# ----------------------------------------------------------------------------
+
+class TestPowderPatternDiffraction(unittest.TestCase):
+
+    pass
+    #def test___init__(self):  assert False
+    #def test_ExtractLeBail(self):  assert False
+    #def test_GetExtractionMode(self):  assert False
+    #def test_GetNbReflBelowMaxSinThetaOvLambda(self):  assert False
+    #def test_GetPowderPatternCalc(self):  assert False
+    #def test_GetProfile(self):  assert False
+    #def test_SetCrystal(self):  assert False
+    #def test_SetExtractionMode(self):  assert False
+    #def test_SetReflectionProfilePar(self):  assert False
+
+# End of class TestPowderPatternDiffraction
+
+# ----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
