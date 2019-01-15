51af1cf00029a3d6777875f505d5e837cd696e9a
Pavol Juhas <pavol.juhas@gmail.com>
2019-01-15 13:53:34 -0500

    BLD: build with conda c++ toolchain

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CPATH="${PREFIX}/include:$CPATH"
-export LIBRARY_PATH="${PREFIX}/lib:$LIBRARY_PATH"
-
 MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 if [ `uname` == Darwin ]; then
@@ -14,8 +11,3 @@ cp ${RECIPE_DIR}/sconscript.local ./
 
 # Install package with scons to utilize multiple CPUs.
 scons -j $MYNCPU install prefix=$PREFIX
-
-# Add more build steps here, if they are necessary.
-
-# See http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
