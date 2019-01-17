7df31c242bee24ee8edf757ba2343fe2a0d8ccf8
Pavol Juhas <pavol.juhas@gmail.com>
2019-01-17 17:29:27 -0500

    MAINT: drop redundant variable from conda script
    DYLD_FALLBACK_LIBRARY_PATH does not make difference for conda build.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,10 +2,6 @@
 
 MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
-if [ `uname` == Darwin ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-fi
-
 # Apply sconscript.local customizations.
 cp ${RECIPE_DIR}/sconscript.local ./
 
