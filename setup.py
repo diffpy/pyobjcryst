d91dadd8d93d561e7652470db73237ddd8aeae64
st3107 <st3107@columbia.edu>
2021-06-08 14:45:20 -0400

    BLD: update FALLBACK_VERSION = '2021.1.1.post0'

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from numpy.distutils.misc_util import get_numpy_include_dirs
 
 # Use this version when git data are not available as in a git zip archive.
 # Update when tagging a new release.
-FALLBACK_VERSION = '2.1.0.post0'
+FALLBACK_VERSION = '2021.1.1.post0'
 
 # define extension arguments here
 ext_kws = {
