66964c3db95b2e823ca2a68690dc7ee9e1a74232
Vincent Favre-Nicolin <favre@esrf.fr>
2022-08-21 20:47:01 +0200

    Version 2.2.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from numpy.distutils.misc_util import get_numpy_include_dirs
 
 # Use this version when git data are not available as in a git zip archive.
 # Update when tagging a new release.
-FALLBACK_VERSION = '2.2.1.post0'
+FALLBACK_VERSION = '2.2.2'
 
 # define extension arguments here
 ext_kws = {
