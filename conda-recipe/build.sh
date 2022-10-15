1bd611d941ef3b233a34aef9027935d8eecaa700
Vincent Favre-Nicolin <favre@esrf.fr>
2022-10-15 20:58:02 +0200

    Remove build.sh for conda recipe

--- conda-recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
-
-# Apply sconscript.local customizations.
-cp ${RECIPE_DIR}/sconscript.local ./
-
-# Install package with scons to utilize multiple CPUs.
-scons -j $MYNCPU install prefix=$PREFIX
