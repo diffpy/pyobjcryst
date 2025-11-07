a468e229eed4a1d0b31fb815a4d24f30f8e33a5b
Edoardo Zatterin <edoardo.zatterin@momentum-transfer.com>
2025-11-07 15:00:53 +0100

    Revert "fix: formatting" for clarity in PR.
    This reverts commit 7253d990bdd73cc2c8a8452c99fb95bcd6b9872d.

--- src/extensions/powderpatterndiffraction_ext.cpp
+++ src/extensions/powderpatterndiffraction_ext.cpp
@@ -1,21 +1,21 @@
 /*****************************************************************************
- *
- * pyobjcryst
- *
- * File coded by:    Vincent Favre-Nicolin
- *
- * See AUTHORS.txt for a list of people who contributed.
- * See LICENSE.txt for license information.
- *
- ******************************************************************************
- *
- * boost::python bindings to ObjCryst::PowderPatternDiffraction.
- *
- * Changes from ObjCryst::PowderPatternDiffraction
- *
- * Other Changes
- *
- *****************************************************************************/
+*
+* pyobjcryst
+*
+* File coded by:    Vincent Favre-Nicolin
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* boost::python bindings to ObjCryst::PowderPatternDiffraction.
+*
+* Changes from ObjCryst::PowderPatternDiffraction
+*
+* Other Changes
+*
+*****************************************************************************/
 
 #include <boost/python/enum.hpp>
 #include <boost/python/class.hpp>
@@ -25,52 +25,56 @@
 
 #include <ObjCryst/ObjCryst/General.h>
 #include <ObjCryst/ObjCryst/PowderPattern.h>
-#include <ObjCryst/ObjCryst/ScatteringData.h>
+#include <ObjCryst/ObjCryst/ScatteringData.h> 
 
 namespace bp = boost::python;
 using namespace boost::python;
 using namespace ObjCryst;
 
+
 void wrap_powderpatterndiffraction()
 {
-        enum_<ReflectionProfileType>("ReflectionProfileType")
-            .value("PROFILE_GAUSSIAN", PROFILE_GAUSSIAN)
-            .value("PROFILE_LORENTZIAN", PROFILE_LORENTZIAN)
-            .value("PROFILE_PSEUDO_VOIGT", PROFILE_PSEUDO_VOIGT)
-            .value("PROFILE_PSEUDO_VOIGT_FINGER_COX_JEPHCOAT",
-                   PROFILE_PSEUDO_VOIGT_FINGER_COX_JEPHCOAT)
-            .value("PROFILE_PEARSON_VII", PROFILE_PEARSON_VII);
+    enum_<ReflectionProfileType>("ReflectionProfileType")
+        .value("PROFILE_GAUSSIAN", PROFILE_GAUSSIAN)
+        .value("PROFILE_LORENTZIAN", PROFILE_LORENTZIAN)
+        .value("PROFILE_PSEUDO_VOIGT", PROFILE_PSEUDO_VOIGT)
+        .value("PROFILE_PSEUDO_VOIGT_FINGER_COX_JEPHCOAT",
+                PROFILE_PSEUDO_VOIGT_FINGER_COX_JEPHCOAT)
+        .value("PROFILE_PEARSON_VII", PROFILE_PEARSON_VII)
+        ;
 
-        class_<PowderPatternDiffraction,
-               bases<PowderPatternComponent, ScatteringData>>(
-            "PowderPatternDiffraction", no_init)
-            .def("GetPowderPatternCalc",
-                 &PowderPatternDiffraction::GetPowderPatternCalc,
-                 return_value_policy<copy_const_reference>())
-            .def("SetReflectionProfilePar",
-                 &PowderPatternDiffraction::SetReflectionProfilePar,
-                 (bp::arg("type") = PROFILE_PSEUDO_VOIGT,
-                  bp::arg("fwhmCagliotiW") = 1e-6,
-                  bp::arg("fwhmCagliotiU") = 0,
-                  bp::arg("fwhmCagliotiV") = 0,
-                  bp::arg("eta0") = 0.5, bp::arg("eta1") = 0))
-            .def("GetProfile",
-                 (ReflectionProfile & (PowderPatternDiffraction::*)()) & PowderPatternDiffraction::GetProfile,
-                 return_internal_reference<>())
-            .def("SetExtractionMode",
-                 &PowderPatternDiffraction::SetExtractionMode,
-                 (bp::arg("extract") = true, bp::arg("init") = false))
-            .def("GetExtractionMode",
-                 &PowderPatternDiffraction::GetExtractionMode)
-            .def("ExtractLeBail",
-                 &PowderPatternDiffraction::ExtractLeBail,
-                 (bp::arg("nbcycle") = 1))
-            .def("SetCrystal",
-                 &PowderPatternDiffraction::SetCrystal,
-                 bp::arg("crystal"),
-                 with_custodian_and_ward<1, 2>())
-            .def("GetNbReflBelowMaxSinThetaOvLambda",
-                 &PowderPatternDiffraction::GetNbReflBelowMaxSinThetaOvLambda)
-            .def("GetFhklObsSq", &PowderPatternDiffraction::GetFhklObsSq,
-                 return_value_policy<copy_const_reference>());
+    class_<PowderPatternDiffraction,
+        bases<PowderPatternComponent, ScatteringData> >(
+                "PowderPatternDiffraction", no_init)
+        .def("GetPowderPatternCalc",
+                &PowderPatternDiffraction::GetPowderPatternCalc,
+                return_value_policy<copy_const_reference>())
+        .def("SetReflectionProfilePar",
+                &PowderPatternDiffraction::SetReflectionProfilePar,
+                (bp::arg("type")=PROFILE_PSEUDO_VOIGT,
+                 bp::arg("fwhmCagliotiW")=1e-6,
+                 bp::arg("fwhmCagliotiU")=0,
+                 bp::arg("fwhmCagliotiV")=0,
+                 bp::arg("eta0")=0.5, bp::arg("eta1")=0))
+        .def("GetProfile",
+                (ReflectionProfile& (PowderPatternDiffraction::*)())
+                &PowderPatternDiffraction::GetProfile,
+                return_internal_reference<>())
+        .def("SetExtractionMode",
+                &PowderPatternDiffraction::SetExtractionMode,
+                (bp::arg("extract")=true, bp::arg("init")=false))
+        .def("GetExtractionMode",
+                &PowderPatternDiffraction::GetExtractionMode)
+        .def("ExtractLeBail",
+                &PowderPatternDiffraction::ExtractLeBail,
+                (bp::arg("nbcycle")=1))
+        .def("SetCrystal",
+                &PowderPatternDiffraction::SetCrystal,
+                bp::arg("crystal"),
+                with_custodian_and_ward<1, 2>())
+        .def("GetNbReflBelowMaxSinThetaOvLambda",
+                &PowderPatternDiffraction::GetNbReflBelowMaxSinThetaOvLambda)
+        .def("GetFhklObsSq", &PowderPatternDiffraction::GetFhklObsSq,
+                return_value_policy<copy_const_reference>())
+        ;
 }
